dbafb71c2d901d9f4abb77771f642347cd5b2383
Vlad Minaev <minaev@haulmont.com>
2019-11-01 23:15:45 +0400

    Make generation work

--- modules/core/src/com/haulmont/addon/datagen/service/DataGenerationServiceBean.kt
+++ modules/core/src/com/haulmont/addon/datagen/service/DataGenerationServiceBean.kt
@@ -1,11 +1,34 @@
 package com.haulmont.addon.datagen.service;
 
 import com.haulmont.addon.datagen.entity.DataGenerationCommand
+import com.haulmont.cuba.core.entity.Entity
+import com.haulmont.cuba.core.global.CommitContext
+import com.haulmont.cuba.core.global.DataManager
+import com.haulmont.cuba.core.global.Metadata
 import org.springframework.stereotype.Service
+import javax.inject.Inject
 
 @Service(DataGenerationService.NAME)
 class DataGenerationServiceBean : DataGenerationService {
-    override fun generateEntities(command: DataGenerationCommand) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    @Inject
+    private lateinit var metadata: Metadata
+    @Inject
+    private lateinit var dataManager: DataManager
+
+    override fun generateEntities(command: DataGenerationCommand, dryRun: Boolean): List<Entity<Any>> {
+        val generatedEntities = generate(command);
+        return if (dryRun) {
+            generatedEntities
+        } else {
+            dataManager.commit(CommitContext(generatedEntities)).toList()
+        }
+    }
+
+    private fun generate(command: DataGenerationCommand): List<Entity<Any>> {
+        val entityGenerationSettings = command.entityGenerationSettings!!
+        return List(entityGenerationSettings.amount!!) {
+            metadata.create(entityGenerationSettings.entityMetaClass)
+        }
     }
 }
--- modules/global/src/com/haulmont/addon/datagen/service/DataGenerationService.kt
+++ modules/global/src/com/haulmont/addon/datagen/service/DataGenerationService.kt
@@ -1,11 +1,12 @@
 package com.haulmont.addon.datagen.service;
 
 import com.haulmont.addon.datagen.entity.DataGenerationCommand
+import com.haulmont.cuba.core.entity.Entity
 
 interface DataGenerationService {
     companion object {
         const val NAME = "datagen_DataGenerationService"
     }
 
-    fun generateEntities(command: DataGenerationCommand)
+    fun generateEntities(command: DataGenerationCommand, dryRun: Boolean = false): List<Entity<Any>>
 }
